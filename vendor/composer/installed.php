4ee2f72b61f90e29aa73ea4f2d3a5555b2968ea2
tsukasa-kiyofuji <tsukasa.wdpro@gmail.com>
2025-02-22 23:43:50 +0900

    add

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'your-vendor-name/your-project-name',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1835d8c998c35c606702bc505e63c4a56c144c0f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'your-vendor-name/your-project-name' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1835d8c998c35c606702bc505e63c4a56c144c0f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
